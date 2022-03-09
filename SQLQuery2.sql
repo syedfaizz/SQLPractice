105c4c40b489c7e96ff28f1ae7682275c769c904
Sayed Faisal <86817472+syedfaizz@users.noreply.github.com>
2022-03-09 22:40:35 +0530

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -47,5 +47,15 @@ VALUES
    select * from students
 
    --using Primary Key--
+    --using Primary Key --
+   create table department(d_id int primary key,d_name varchar(40),d_address varchar(50))--parent table--
+insert department values(10,'.net','hyderabad'),(20,'java','pune'),(30,'python','mubai')
+create table employee2(emp_id int,emp_name varchar(40),emp_salary money,
+d_id int foreign key references department(d_id))--child class--
+select*from department 
+select*from employee2
+insert employee2 values(101,'faizz',36450,10)
+insert employee2 values(102,'arjun',25410,20)
+insert employee2 values (103,'Amit',56412,30)
 
  
